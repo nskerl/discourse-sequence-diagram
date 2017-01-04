85f0dfbc6b141fa1d81c7459bda17152703b71dc
Nathan Skerl <nskerl@gmail.com>
2017-01-04 11:24:07 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,7 +3,7 @@
 # version: 0.1
 # authors: Nathan Skerl
 
-register_asset "javascripts/raphael-min.js"
+register_asset "javascripts/snap.svg-min.js"
 register_asset "javascripts/sequence-diagram-min.js"
 register_asset "javascripts/initializers/sequence-diagram.js.es6"
 register_asset "javascripts/sequence_diagram_dialect.js", :server_side
