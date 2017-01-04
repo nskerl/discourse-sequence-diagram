c86f45b4cf0bd8305b9461fd4bebdc6342e818b0
Nathan Skerl <nskerl@gmail.com>
2017-01-04 13:39:11 -0800

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,6 +3,8 @@
 # version: 0.1
 # authors: Nathan Skerl
 
+enabled_site_setting :diagrams_plugin_enabled
+
 register_asset "javascripts/snap.svg-min.js"
 register_asset "javascripts/sequence-diagram-min.js"
 register_asset "javascripts/initializers/sequence-diagram.js.es6"
